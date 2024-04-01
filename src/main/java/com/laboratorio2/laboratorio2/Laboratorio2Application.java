ca3b182c51b3862dbd53b56cf38b21464b006625
ValenMR <valentina.munozr1@udea.edu.co>
2024-04-01 12:14:46 -0500

    Actualizando archivos añadidos

---
+++ src/main/java/com/laboratorio2/laboratorio2/Laboratorio2Application.java
@@ -0,0 +1,13 @@
+package com.laboratorio2.laboratorio2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Laboratorio2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Laboratorio2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/laboratorio2/laboratorio2/Laboratorio2ApplicationTests.java
@@ -0,0 +1,46 @@
+package com.laboratorio2.laboratorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Laboratorio2ApplicationTests {
+//cablear o inyectar el datacontroller acá
+
+	@Autowired
+	DataController dataController;
+
+	@Test
+	void health() {
+		assertEquals("APLICACION FUNCIONANDO OK", dataController.healthCheck());
+	}
+
+	@Test
+	void version() {
+		assertEquals("La version Actual es 1.0.0", dataController.version());
+	}
+
+	@Test
+	void nationsLength() {
+		Integer nationsLength = dataController.getRandomNations().size();
+		assertEquals(10, nationsLength);
+	}
+
+	@Test
+	void currenciesLength() {
+		Integer currenciesLength = dataController.getRandomNations().size();
+		assertEquals(10, currenciesLength);
+	}
+
+	@Test
+	void airlinesLength() {
+		Integer airlinesLength = dataController.getRandomNations().size();
+		assertEquals(10, airlinesLength);
+
+	}
+
+
+}
